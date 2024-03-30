4f48e68c7681ae22cf950e3ccd7f60c20632c255
manav100 <fatihtutar93@gmail.com>
2024-03-30 18:22:40 +0300

    Bazı maddeler çıkarıldı.

--- template/banner.php
+++ template/banner.php
@@ -1,8 +1,8 @@
-<div class="container-fluid" style="position: fixed; z-index: 2; background-color: white;">
+<?php if($girdi == 1){ ?>
 
-	<div class="row">
+	<div class="container-fluid" style="position: fixed; z-index: 5; background-color: black;">
 
-		<?php if($girdi == 1){ ?>
+		<div class="row">
 
 			<div class="col-xl-2 col-lg-3 col-md-4 col-sm-5 col-7" style="text-align: left; padding-top: 10px; padding-bottom: 10px;">
 
@@ -10,47 +10,69 @@
 
 			</div>
 
-			<div class="col-xl-10 col-lg-9 col-md-8 col-sm-7 col-5" style="text-align: right;">
+			<div class="col-xl-10 col-lg-9 col-md-8 col-sm-7 col-5 d-flex align-items-center justify-content-end" style="text-align: right;">
 
 				<div class="dropdown" style="margin: 10px;">
 				
-					<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<button class="btn btn-primary dropdown-toggle" style="background-color:black; border-style: none;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				
-						Menü
+						<?= $uye_adi ?>
 				
 					</button>
 				
 					<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 				
-						<a class="dropdown-item" href="index.php" style="margin: 10px;"><b>ANA SAYFA</b></a>
+						<a class="dropdown-item" href="index.php"><b>ANA SAYFA</b></a>
 				
-						<a class="dropdown-item" href="profil.php?id=<?php echo $uye_id; ?>" style="margin: 10px;"><b>PROFİL</b></a>
+						<a class="dropdown-item" href="profil.php?id=<?php echo $uye_id; ?>"><b>PROFİL</b></a>
 				
 						<?php if($uye_tipi == '2' || $uye_tipi == '1' || $uye_tipi == '3'){?>
 				
 							<?php if($uye_tipi == '2'){?>
 
-								<a class="dropdown-item" href="yonetim.php" style="margin: 10px;"><b>YÖNETİM</b></a>
+								<hr class="m-1"/>
+
+								<a class="dropdown-item" href="fiyatlar.php"><b>FİYATLAR</b></a>
+
+								<a class="dropdown-item" href="plan.php"><b>PLAN</b></a>
+
+								<a class="dropdown-item" href="yonetim.php"><b>YÖNETİM</b></a>
+
+								<hr class="m-1"/>
+
+							<?php } ?>
+
+							<?php if($uye_ziyaret_yetkisi == '1'){?>
+
+								<a class="dropdown-item" href="ziyaretler.php"><b>ZİYARETLER</b></a>
+
+							<?php } ?>
+
+							<?php if($uye_gelen_giden_yetkisi == '1'){?>
+
+								<a class="dropdown-item" href="gelengiden.php"><b>GELEN/GİDEN</b></a>
 
 							<?php } ?>
 
 							<?php if($uye_islemleri_gorme_yetkisi == '1'){?>
 
-								<a class="dropdown-item" href="islemler.php" style="margin: 10px;"><b>İŞLEMLER</b></a>
+								<a class="dropdown-item" href="islemler.php"><b>İŞLEMLER</b></a>
 
 							<?php } ?>
 
-							<?php if($uye_tipi == '2' || $uye_tipi == '1'){?><a class="dropdown-item" href="kategoriler.php" style="margin: 10px;"><b>KATEGORİLER</b></a><?php } ?>
+							<?php if($uye_tipi == '2' || $uye_tipi == '1' || $uye_tipi == '3'){?>
+								
+								<a class="dropdown-item" href="kategoriler.php"><b>KATEGORİLER</b></a>
+							
+							<?php } ?>
 
-							<a class="dropdown-item" href="fabrikalar.php" style="margin: 10px;"><b>FABRİKALAR</b></a>
+							<hr class="m-1"/>
 
-							<a class="dropdown-item" href="firmalar.php" style="margin: 10px;"><b>FİRMALAR</b></a>
-
-							<a class="dropdown-item" href="kaliplar.php" style="margin: 10px;"><b>KALIPLAR</b></a>
+							<a class="dropdown-item" href="yardim.php"><b>YARDIM</b></a>
 					
 						<?php } ?>
 
-						<a class="dropdown-item" href="cikis.php" style="margin: 10px;"><b>ÇIKIŞ</b></a>
+						<a class="dropdown-item" href="cikis.php"><b>ÇIKIŞ</b></a>
 				
 					</div>
 				
@@ -58,7 +80,15 @@
 
 			</div>
 
-		<?php }elseif ($girdi == 0) { ?>
+		</div>
+
+	</div>
+
+<?php }elseif ($girdi == 0) { ?>
+
+	<div class="container-fluid" style="position: fixed; z-index: 2; background-color: white;">
+
+		<div class="row">
 
 			<div class="col-xl-10 col-lg-8 col-md-6 col-sm-6 col-6" style="text-align: left; padding-top: 10px; padding-bottom: 10px;">
 
@@ -68,19 +98,19 @@
 
 			<div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 col-6" style="text-align: right; padding-top: 30px;">
 
-				<div class="btn-group" role="group" aria-label="Basic example">
+				<!--<div class="btn-group" role="group" aria-label="Basic example">
 					
 					<button type="button" class="btn btn-danger" onclick="location.href='uyeol.php'"><i class="fa fa-user-plus"></i>&nbsp;&nbsp;&nbsp;Üye Ol</button>
 				
-				</div>
+				</div>-->
 
 			</div>
 
-		<?php } ?>
+		</div>
 
 	</div>
 
-</div>
+<?php } ?>
 
 <br/><br/><br/><br/>
 
